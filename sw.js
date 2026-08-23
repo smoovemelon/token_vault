4e74b7fb612c62dfbe70b0ccd551d02136b6b239
Adrinn Nguyen-Sene <adrinn.nguyensene@gmail.com>
2026-08-23 12:50:39 -0500

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'toro-v6';
+const ASSETS = ['/index.html', '/style.css', '/app.js', '/icon.png', '/manifest.json'];
+
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
